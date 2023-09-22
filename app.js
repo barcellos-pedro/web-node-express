e53452e958d8bbe92ecc74a757a57e37cde750d8
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-09-21 21:29:44 -0300

    Add newsletter handlers and views

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const handlers = require("./lib/handlers")
-const handlebars = require('./lib/view-engine')
+const handlebars = require("./lib/view-engine")
 const weatherData = require("./lib/middleware/weather")
 
 const app = express()
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 3000
 const isMainModule = require.main === module
 
 /** Config */
-app.disable('x-powered-by')
+app.disable("x-powered-by")
 
 app.use(express.static(`${__dirname}/public`))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(weatherData)
 
 app.engine("handlebars", handlebars.engine)
@@ -20,7 +22,14 @@ app.set("view engine", "handlebars")
 app.get("/", handlers.home)
 app.get("/about", handlers.about)
 
-/** Middlewares */
+app.get("newsletter", handlers.newsletter)
+
+app.get("newsletter-signup", handlers.newsletterSignup)
+app.post("newsletter-signup/process", handlers.newsletterSignupProcess)
+app.get("newsletter-signup/thank-you", handlers.newsletterSignupThanks)
+
+/** API Endpoints */
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
 // Not Found
 app.use(handlers.notFound)
